0d09639ec9e47ef92db38a4f582741915fd4ffbb
ohakutsu <dev@ohakutsu.com>
2022-03-24 14:59:36 +0900

    Fix ffmpeg shortcut

--- .zsh/ffmpeg.zsh
+++ .zsh/ffmpeg.zsh
@@ -1,6 +1,6 @@
 fp() {
-	mov_file = $1
-	gif_file = $2
+	mov_file=$1
+	gif_file=$2
 
-	ffmpeg -i $1 -filter_complex "[0:v] fps=10,scale=640:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" $2
+	ffmpeg -i $mov_file -filter_complex "[0:v] fps=10,scale=640:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" $gif_file
 }
