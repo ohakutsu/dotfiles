e592019ffcae637db6b69e600cc90e52e9ebfb22
ohakutsu <dev@ohakutsu.com>
2022-05-28 14:19:00 +0900

    Update aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,10 +1,13 @@
 # Aliases
-alias rm='rm -i'
-alias mv='mv -i'
-alias cp='cp -i'
+# Base: https://github.com/ohmyzsh/ohmyzsh/blob/e9e8c6b54d594109041bdd4bc3902b40f9ae8849/plugins/common-aliases/common-aliases.plugin.zsh
 
-alias lsa='ls -Glah'
-alias l='ls -Glah'
-alias ll='ls -Glh'
-alias la='ls -GlAh'
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias l='ls -GlFh'   #size,show type,human readable
+alias la='ls -GlAFh' #long list,show almost all,show type,human readable
+alias lt='ls -GltFh' #long list,sorted by date,show type,human readable
+alias ll='ls -Gl'    #long list
+alias ldot='ls -Gld .*'
 alias ls='ls -G'
