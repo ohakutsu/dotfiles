dc60080ec28ff5b7726dbfdafdaa62c20e0ae46f
ohakutsu <dev@ohakutsu.com>
2023-02-05 15:37:54 +0900

    Fix anyenv.zsh

--- .zsh/anyenv.zsh
+++ .zsh/anyenv.zsh
@@ -1,2 +1,7 @@
-eval "$(nodenv init -)"
-eval "$(rbenv init - zsh)"
+if which nodenv 1>/dev/null 2>&1; then
+	eval "$(nodenv init -)"
+fi
+
+if which rbenv 1>/dev/null 2>&1; then
+	eval "$(rbenv init - zsh)"
+fi
