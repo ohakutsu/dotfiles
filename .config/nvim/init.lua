c59e75e1d7ffc0fbe7ee6920c7280288c4142b95
ohakutsu <dev@ohakutsu.com>
2023-08-15 14:02:46 +0900

    Update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,7 @@ require("user.lsp")
 require("user.telescope")
 require("user.treesitter")
 require("user.nvim-comment")
+require("user.socks-copy-path")
 
 --[[ vim config ]]
 vim.opt.autoindent = true
