eff950839f5f8e32de01e134dcb0267ad6804e05
ohakutsu <dev@ohakutsu.com>
2023-10-10 12:55:57 +0900

    Update ffmpeg.zsh

--- .zsh/ffmpeg.zsh
+++ .zsh/ffmpeg.zsh
@@ -2,5 +2,5 @@ fp() {
 	mov_file=$1
 	gif_file=$2
 
-	ffmpeg -i $mov_file -filter_complex "[0:v] fps=10,scale=640:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" $gif_file
+	ffmpeg -i $mov_file -filter_complex "[0:v] fps=10,scale=1080:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" $gif_file
 }
