6b1de5c5a153416f0739ebd5a48e6e6872b1b1db
ohakutsu <dev@ohakutsu.com>
2023-12-28 22:01:15 +0900

    Set cspell language id

--- .config/nvim/lua/user/plugin-configs/ale.lua
+++ .config/nvim/lua/user/plugin-configs/ale.lua
@@ -37,3 +37,16 @@ vim.g.ale_ruby_rubocop_auto_correct_all = 1
 -- vim.g.ale_slim_slim_lint_executable = "bundle exec slim-lint"
 
 -- TODO: sql-formatter
+
+-- cspell
+local cspell_group = vim.api.nvim_create_augroup("AleCspellGroup", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  group = cspell_group,
+  pattern = "*",
+  callback = function(event)
+    local filetype = vim.bo.filetype
+    if filetype ~= "" then
+      vim.api.nvim_buf_set_var(event["buf"], "ale_cspell_options", string.format("--language-id '%s'", filetype))
+    end
+  end,
+})
